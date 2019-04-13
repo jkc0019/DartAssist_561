edb667f339c998c20d913dd137ed39a00f615637
Altiss <kerry.l.doan@gmail.com>
2019-04-12 18:20:57 -0600

    Better logic for dealing with 1 and 2 dart outs.

---
+++ DartAssistant/DartAssistant/DartAssistant/TwoDartOuts.cs
@@ -0,0 +1,572 @@
+﻿using System.Collections.Generic;
+
+namespace DartAssistant
+{
+    internal class TwoDartOuts: Dictionary<int, List<Dart>>
+    {
+        public TwoDartOuts()
+        {
+            Add(100, new List<Dart>
+            {
+                new Dart(20, SegmentMultiplier.Triple),
+                new Dart(20, SegmentMultiplier.Double)
+            });
+
+            Add(98, new List<Dart>
+            {
+                new Dart(20, SegmentMultiplier.Triple),
+                new Dart(19, SegmentMultiplier.Double)
+            });
+
+            Add(97, new List<Dart>
+            {
+                new Dart(19, SegmentMultiplier.Triple),
+                new Dart(20, SegmentMultiplier.Double)
+            });
+
+            Add(96, new List<Dart>
+            {
+                new Dart(20, SegmentMultiplier.Triple),
+                new Dart(18, SegmentMultiplier.Double)
+            });
+
+            Add(94, new List<Dart>
+            {
+                new Dart(18, SegmentMultiplier.Triple),
+                new Dart(20, SegmentMultiplier.Double)
+            });
+
+            Add(93, new List<Dart>
+            {
+                new Dart(19, SegmentMultiplier.Triple),
+                new Dart(18, SegmentMultiplier.Double)
+            });
+
+            Add(92, new List<Dart>
+            {
+                new Dart(20, SegmentMultiplier.Triple),
+                new Dart(16, SegmentMultiplier.Double)
+            });
+
+            Add(91, new List<Dart>
+            {
+                new Dart(17, SegmentMultiplier.Triple),
+                new Dart(20, SegmentMultiplier.Double)
+            });
+
+            Add(90, new List<Dart>
+            {
+                new Dart(18, SegmentMultiplier.Triple),
+                new Dart(18, SegmentMultiplier.Double)
+            });
+
+            Add(89, new List<Dart>
+            {
+                new Dart(19, SegmentMultiplier.Triple),
+                new Dart(16, SegmentMultiplier.Double)
+            });
+
+            Add(88, new List<Dart>
+            {
+                new Dart(16, SegmentMultiplier.Triple),
+                new Dart(20, SegmentMultiplier.Double)
+            });
+
+            Add(87, new List<Dart>
+            {
+                new Dart(17, SegmentMultiplier.Triple),
+                new Dart(18, SegmentMultiplier.Double)
+            });
+
+            Add(86, new List<Dart>
+            {
+                new Dart(18, SegmentMultiplier.Triple),
+                new Dart(16, SegmentMultiplier.Double)
+            });
+
+            Add(85, new List<Dart>
+            {
+                new Dart(15, SegmentMultiplier.Triple),
+                new Dart(20, SegmentMultiplier.Double)
+            });
+
+            Add(84, new List<Dart>
+            {
+                new Dart(20, SegmentMultiplier.Triple),
+                new Dart(12, SegmentMultiplier.Double)
+            });
+
+            Add(83, new List<Dart>
+            {
+                new Dart(17, SegmentMultiplier.Triple),
+                new Dart(16, SegmentMultiplier.Double)
+            });
+
+            Add(82, new List<Dart>
+            {
+                new Dart(14, SegmentMultiplier.Triple),
+                new Dart(20, SegmentMultiplier.Double)
+            });
+
+            Add(81, new List<Dart>
+            {
+                new Dart(15, SegmentMultiplier.Triple),
+                new Dart(18, SegmentMultiplier.Double)
+            });
+
+            Add(80, new List<Dart>
+            {
+                new Dart(16, SegmentMultiplier.Triple),
+                new Dart(16, SegmentMultiplier.Double)
+            });
+
+            Add(79, new List<Dart>
+            {
+                new Dart(13, SegmentMultiplier.Triple),
+                new Dart(20, SegmentMultiplier.Double)
+            });
+
+            Add(78, new List<Dart>
+            {
+                new Dart(14, SegmentMultiplier.Triple),
+                new Dart(18, SegmentMultiplier.Double)
+            });
+
+            Add(77, new List<Dart>
+            {
+                new Dart(15, SegmentMultiplier.Triple),
+                new Dart(16, SegmentMultiplier.Double)
+            });
+
+            Add(76, new List<Dart>
+            {
+                new Dart(20, SegmentMultiplier.Triple),
+                new Dart(8, SegmentMultiplier.Double)
+            });
+
+            Add(75, new List<Dart>
+            {
+                new Dart(15, SegmentMultiplier.Triple),
+                new Dart(15, SegmentMultiplier.Double)
+            });
+
+            Add(74, new List<Dart>
+            {
+                new Dart(14, SegmentMultiplier.Triple),
+                new Dart(16, SegmentMultiplier.Double)
+            });
+
+            Add(73, new List<Dart>
+            {
+                new Dart(19, SegmentMultiplier.Triple),
+                new Dart(8, SegmentMultiplier.Double)
+            });
+
+            Add(72, new List<Dart>
+            {
+                new Dart(20, SegmentMultiplier.Triple),
+                new Dart(6, SegmentMultiplier.Double)
+            });
+
+            Add(71, new List<Dart>
+            {
+                new Dart(13, SegmentMultiplier.Triple),
+                new Dart(16, SegmentMultiplier.Double)
+            });
+
+            Add(70, new List<Dart>
+            {
+                new Dart(18, SegmentMultiplier.Triple),
+                new Dart(8, SegmentMultiplier.Double)
+            });
+
+            Add(69, new List<Dart>
+            {
+                new Dart(19, SegmentMultiplier.Triple),
+                new Dart(6, SegmentMultiplier.Double)
+            });
+
+            Add(68, new List<Dart>
+            {
+                new Dart(16, SegmentMultiplier.Triple),
+                new Dart(10, SegmentMultiplier.Double)
+            });
+
+            Add(67, new List<Dart>
+            {
+                new Dart(17, SegmentMultiplier.Triple),
+                new Dart(8, SegmentMultiplier.Double)
+            });
+
+            Add(66, new List<Dart>
+            {
+                new Dart(10, SegmentMultiplier.Triple),
+                new Dart(18, SegmentMultiplier.Double)
+            });
+
+            Add(65, new List<Dart>
+            {
+                new Dart(15, SegmentMultiplier.Triple),
+                new Dart(10, SegmentMultiplier.Double)
+            });
+
+            Add(64, new List<Dart>
+            {
+                new Dart(16, SegmentMultiplier.Double),
+                new Dart(16, SegmentMultiplier.Double)
+            });
+
+            Add(63, new List<Dart>
+            {
+                new Dart(13, SegmentMultiplier.Triple),
+                new Dart(12, SegmentMultiplier.Double)
+            });
+
+            Add(62, new List<Dart>
+            {
+                new Dart(10, SegmentMultiplier.Triple),
+                new Dart(16, SegmentMultiplier.Double)
+            });
+
+            Add(61, new List<Dart>
+            {
+                new Dart(15, SegmentMultiplier.Triple),
+                new Dart(8, SegmentMultiplier.Double)
+            });
+
+            Add(60, new List<Dart>
+            {
+                new Dart(20, SegmentMultiplier.Single),
+                new Dart(20, SegmentMultiplier.Double)
+            });
+
+            Add(59, new List<Dart>
+            {
+                new Dart(19, SegmentMultiplier.Single),
+                new Dart(20, SegmentMultiplier.Double)
+            });
+
+            Add(58, new List<Dart>
+            {
+                new Dart(18, SegmentMultiplier.Single),
+                new Dart(20, SegmentMultiplier.Double)
+            });
+
+            Add(57, new List<Dart>
+            {
+                new Dart(17, SegmentMultiplier.Single),
+                new Dart(20, SegmentMultiplier.Double)
+            });
+
+            Add(56, new List<Dart>
+            {
+                new Dart(16, SegmentMultiplier.Single),
+                new Dart(20, SegmentMultiplier.Double)
+            });
+
+            Add(55, new List<Dart>
+            {
+                new Dart(15, SegmentMultiplier.Single),
+                new Dart(20, SegmentMultiplier.Double)
+            });
+
+            Add(54, new List<Dart>
+            {
+                new Dart(14, SegmentMultiplier.Single),
+                new Dart(20, SegmentMultiplier.Double)
+            });
+
+            Add(53, new List<Dart>
+            {
+                new Dart(13, SegmentMultiplier.Single),
+                new Dart(20, SegmentMultiplier.Double)
+            });
+
+            Add(52, new List<Dart>
+            {
+                new Dart(20, SegmentMultiplier.Single),
+                new Dart(16, SegmentMultiplier.Double)
+            });
+
+            Add(51, new List<Dart>
+            {
+                new Dart(19, SegmentMultiplier.Single),
+                new Dart(16, SegmentMultiplier.Double)
+            });
+
+            Add(50, new List<Dart>
+            {
+                new Dart(18, SegmentMultiplier.Single),
+                new Dart(16, SegmentMultiplier.Double)
+            });
+
+            Add(49, new List<Dart>
+            {
+                new Dart(17, SegmentMultiplier.Single),
+                new Dart(16, SegmentMultiplier.Double)
+            });
+
+            Add(48, new List<Dart>
+            {
+                new Dart(16, SegmentMultiplier.Single),
+                new Dart(16, SegmentMultiplier.Double)
+            });
+
+            Add(47, new List<Dart>
+            {
+                new Dart(15, SegmentMultiplier.Single),
+                new Dart(16, SegmentMultiplier.Double)
+            });
+
+            Add(46, new List<Dart>
+            {
+                new Dart(14, SegmentMultiplier.Single),
+                new Dart(16, SegmentMultiplier.Double)
+            });
+
+            Add(45, new List<Dart>
+            {
+                new Dart(13, SegmentMultiplier.Single),
+                new Dart(16, SegmentMultiplier.Double)
+            });
+
+            Add(44, new List<Dart>
+            {
+                new Dart(12, SegmentMultiplier.Single),
+                new Dart(16, SegmentMultiplier.Double)
+            });
+
+            Add(43, new List<Dart>
+            {
+                new Dart(11, SegmentMultiplier.Single),
+                new Dart(16, SegmentMultiplier.Double)
+            });
+
+            Add(42, new List<Dart>
+            {
+                new Dart(10, SegmentMultiplier.Single),
+                new Dart(16, SegmentMultiplier.Double)
+            });
+
+            Add(41, new List<Dart>
+            {
+                new Dart(9, SegmentMultiplier.Single),
+                new Dart(16, SegmentMultiplier.Double)
+            });
+
+            Add(40, new List<Dart>
+            {
+                new Dart(20, SegmentMultiplier.Double)
+            });
+
+            Add(39, new List<Dart>
+            {
+                new Dart(7, SegmentMultiplier.Single),
+                new Dart(16, SegmentMultiplier.Double)
+            });
+
+            Add(38, new List<Dart>
+            {
+                new Dart(19, SegmentMultiplier.Double)
+            });
+
+            Add(37, new List<Dart>
+            {
+                new Dart(5, SegmentMultiplier.Single),
+                new Dart(16, SegmentMultiplier.Double)
+            });
+
+            Add(36, new List<Dart>
+            {
+                new Dart(18, SegmentMultiplier.Double)
+            });
+
+            Add(35, new List<Dart>
+            {
+                new Dart(3, SegmentMultiplier.Single),
+                new Dart(16, SegmentMultiplier.Double)
+            });
+
+            Add(34, new List<Dart>
+            {
+                new Dart(17, SegmentMultiplier.Double)
+            });
+
+            Add(33, new List<Dart>
+            {
+                new Dart(11, SegmentMultiplier.Single),
+                new Dart(11, SegmentMultiplier.Double)
+            });
+
+            Add(32, new List<Dart>
+            {
+                new Dart(16, SegmentMultiplier.Double)
+            });
+
+            Add(31, new List<Dart>
+            {
+                new Dart(7, SegmentMultiplier.Single),
+                new Dart(12, SegmentMultiplier.Double)
+            });
+
+            Add(30, new List<Dart>
+            {
+                new Dart(15, SegmentMultiplier.Double)
+            });
+
+            Add(29, new List<Dart>
+            {
+                new Dart(13, SegmentMultiplier.Single),
+                new Dart(8, SegmentMultiplier.Double)
+            });
+
+            Add(28, new List<Dart>
+            {
+                new Dart(14, SegmentMultiplier.Double)
+            });
+
+            Add(27, new List<Dart>
+            {
+                new Dart(11, SegmentMultiplier.Single),
+                new Dart(8, SegmentMultiplier.Double)
+            });
+
+            Add(26, new List<Dart>
+            {
+                new Dart(13, SegmentMultiplier.Double)
+            });
+
+            Add(25, new List<Dart>
+            {
+                new Dart(9, SegmentMultiplier.Single),
+                new Dart(8, SegmentMultiplier.Double)
+            });
+
+            Add(24, new List<Dart>
+            {
+                new Dart(12, SegmentMultiplier.Double)
+            });
+
+            Add(23, new List<Dart>
+            {
+                new Dart(7, SegmentMultiplier.Single),
+                new Dart(8, SegmentMultiplier.Double)
+            });
+
+            Add(22, new List<Dart>
+            {
+                new Dart(11, SegmentMultiplier.Double)
+            });
+
+            Add(21, new List<Dart>
+            {
+                new Dart(5, SegmentMultiplier.Single),
+                new Dart(8, SegmentMultiplier.Double)
+            });
+
+            Add(20, new List<Dart>
+            {
+                new Dart(10, SegmentMultiplier.Double)
+            });
+
+            Add(19, new List<Dart>
+            {
+                new Dart(3, SegmentMultiplier.Single),
+                new Dart(8, SegmentMultiplier.Double)
+            });
+
+            Add(18, new List<Dart>
+            {
+                new Dart(9, SegmentMultiplier.Double)
+            });
+
+            Add(17, new List<Dart>
+            {
+                new Dart(9, SegmentMultiplier.Single),
+                new Dart(4, SegmentMultiplier.Double)
+            });
+
+            Add(16, new List<Dart>
+            {
+                new Dart(8, SegmentMultiplier.Double)
+            });
+
+            Add(15, new List<Dart>
+            {
+                new Dart(7, SegmentMultiplier.Single),
+                new Dart(4, SegmentMultiplier.Double)
+            });
+
+            Add(14, new List<Dart>
+            {
+                new Dart(7, SegmentMultiplier.Double)
+            });
+
+            Add(13, new List<Dart>
+            {
+                new Dart(5, SegmentMultiplier.Single),
+                new Dart(4, SegmentMultiplier.Double)
+            });
+
+            Add(12, new List<Dart>
+            {
+                new Dart(6, SegmentMultiplier.Double)
+            });
+
+            Add(11, new List<Dart>
+            {
+                new Dart(3, SegmentMultiplier.Single),
+                new Dart(4, SegmentMultiplier.Double)
+            });
+
+            Add(10, new List<Dart>
+            {
+                new Dart(5, SegmentMultiplier.Double)
+            });
+
+            Add(9, new List<Dart>
+            {
+                new Dart(1, SegmentMultiplier.Single),
+                new Dart(4, SegmentMultiplier.Double)
+            });
+
+            Add(8, new List<Dart>
+            {
+                new Dart(4, SegmentMultiplier.Double)
+            });
+
+            Add(7, new List<Dart>
+            {
+                new Dart(3, SegmentMultiplier.Single),
+                new Dart(2, SegmentMultiplier.Double)
+            });
+
+            Add(6, new List<Dart>
+            {
+                new Dart(3, SegmentMultiplier.Double)
+            });
+
+            Add(5, new List<Dart>
+            {
+                new Dart(1, SegmentMultiplier.Single),
+                new Dart(2, SegmentMultiplier.Double)
+            });
+
+            Add(4, new List<Dart>
+            {
+                new Dart(2, SegmentMultiplier.Double)
+            });
+
+            Add(3, new List<Dart>
+            {
+                new Dart(1, SegmentMultiplier.Single),
+                new Dart(1, SegmentMultiplier.Double)
+            });
+
+            Add(2, new List<Dart>
+            {
+                new Dart(1, SegmentMultiplier.Double)
+            });
+        }
+    }
+}
